8e5ae53050d889e281ba90cf2ee1f60a659faf25
Omerbhatti <Omerbhatti34@gmail.com>
2021-11-05 20:58:51 +0500

    Added share and gobackToPractice Intent on Result Screen

--- app/src/main/java/com/codefumes/alphabetconverter/ResultActivity.java
+++ app/src/main/java/com/codefumes/alphabetconverter/ResultActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -26,4 +27,17 @@ public class ResultActivity extends AppCompatActivity {
         rightTxt.setText(Integer.toString(right));
         wrongTxt.setText(Integer.toString(wrong));
     }
+
+    public void shareResult(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,"I took a test on Alphabet Converter App.\nHere is the result:\nRight = "+Integer.toString(right)+"\nWrong = "+Integer.toString(wrong));
+        startActivity(intent);
+    }
+
+    public void goBacktoPractice(View v){
+        Intent intent = new Intent(this,ConverterActivity.class);
+        startActivity(intent);
+    }
+
 }
